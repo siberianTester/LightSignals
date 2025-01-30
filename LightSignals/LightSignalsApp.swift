26ee7028773a6371752f8fcb57680b4d5ef33ed9
Yaroslav Malygin <ya.malygin@team.superjob.ru>
2025-01-30 20:28:29 +0300

    Initial Commit

---
+++ LightSignals/LightSignalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightSignalsApp.swift
+//  LightSignals
+//
+//  Created by Yaroslav Malygin on 30.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct LightSignalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
